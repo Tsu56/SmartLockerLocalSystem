52b89cb01d584b17f5febd5d003ebabc1196d87e
Phukritsakon <supawitchtt@gmail.com>
2025-10-18 11:39:35 +0700

    เตรียม device_identity_service

--- device_identity_service/app.py
+++ device_identity_service/app.py
@@ -1,26 +1,9 @@
-from flask import Flask, jsonify, request
-import sqlite3
-
+from flask import Flask
 app = Flask(__name__)
-DB = "database/device_identity.db"
 
-@app.route('/locations', methods=['GET'])
-def get_locations():
-    conn = sqlite3.connect(DB)
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM locations")
-    data = cursor.fetchall()
-    conn.close()
-    return jsonify(data)
-
-@app.route('/lockers', methods=['GET'])
-def get_lockers():
-    conn = sqlite3.connect(DB)
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM lockers")
-    data = cursor.fetchall()
-    conn.close()
-    return jsonify(data)
+@app.route('/')
+def home():
+    return "<h1>Device Identity Service is Running</h1>"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
